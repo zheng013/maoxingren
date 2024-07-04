837dad2232a997e91029e3da9d7184c28d81908d
zhenglijian <zhenglj@ihome-iot.com>
2024-07-04 15:07:02 +0800

    feat():完成初版页面开发。[]

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,7 @@
-module.exports = {}
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,52 @@
-import { View, Text } from '@tarojs/components'
-
+import { View, Image, Text, Input } from '@tarojs/components'
+import LoadingProgressView from '../../components/loading-process'
 import './index.less'
+import Taro from '@tarojs/taro'
+import { useState } from 'react'
 
 
 function Index() {
+  const [loading, setLoading] = useState(false)
+  const [source, setSource] = useState<string | null>(null);
+
+  const sendMessage = () => {
+    setLoading(true)
+    Taro.showToast({
+      title: '发送成功'
+    })
+  }
+
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
-      <Text>Hello world!</Text>
-      <Text>Hello world!</Text>
-      <Text>Hello world!</Text>
-      <Text>{111}</Text>
+      <View className='control'>
+        {loading && <LoadingProgressView className='loading' setLoading={setLoading} />}
+        {source && <Image className='img-bg' src={source} />}
+        <Image className='img-bg' src={require('./assets/bg.png')} />
+        <View className='title-pannel'>
+          <View className='avatar' >
+            <View className='avatar-logo'></View>
+            <Text>swift paul132</Text>
+          </View>
+          <Image className='img-icon' src={require('./assets/close.png')} />
+        </View>
+        <View className='handler-pannel'>
+          <Image className='img-icon-camera' onClick={() => {
+            Taro.chooseImage({
+              count: 3,
+              sourceType: ['album'],
+              success: res => {
+                // setTempFilePath(res.tempFilePaths);
+                setSource(res.tempFilePaths[0])
+              },
+              fail: err => {
+                console.log(err);
+              }
+            })
+          }} src={require('./assets/camera.png')} />
+          <Input className='input' placeholder='send message'></Input>
+          <Image onClick={sendMessage} className='img-icon' src={require('./assets/send.png')} />
+        </View>
+      </View>
     </View>
   )
 }
