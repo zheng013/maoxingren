cd569031296fb215047987b8688c0a331f0f0e34
zhenglijian <zhenglj@ihome-iot.com>
2024-07-04 18:04:10 +0800

    feat():安卓打包目录替换。[]

---
+++ android/app/src/main/java/com/tarodemo/devmanager/TaroReactNativeHost.kt
@@ -0,0 +1,22 @@
+package com.maoxingren.devmanager
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+
+open class TaroReactNativeHost(application: Application) : ReactNativeHost(application) {
+
+    var jsMainModulePath: String? = "index"
+
+    override fun getUseDeveloperSupport() = true
+
+    override fun getJSMainModuleName() = jsMainModulePath
+
+    override fun getPackages(): List<ReactPackage>? {
+        return PackageList(this).packages.apply {
+            add(DevManagerPackage())
+        }
+    }
+
+}
